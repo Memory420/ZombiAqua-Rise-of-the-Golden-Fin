d32497234a199b21ae589bef312cb5a04b70bc93
Memory420 <ihatesspies@gmail.com>
2024-04-03 19:39:04 +0300

    Added click sound to menu

--- ZombiAqua Rise of the Golden-Fin/Assets/Scripts/CustomButton.cs
+++ ZombiAqua Rise of the Golden-Fin/Assets/Scripts/CustomButton.cs
@@ -6,20 +6,31 @@ public class CustomButton : MonoBehaviour
     public Sprite defaultSprite;
     public Sprite pressedSprite;
     public UnityEvent onPressed;
+    public AudioClip clickSound;
 
     private SpriteRenderer spriteRenderer;
     private bool isPressed = false;
+    private AudioSource audioSource;
 
     void Awake()
     {
         spriteRenderer = GetComponent<SpriteRenderer>();
         spriteRenderer.sprite = defaultSprite;
+        audioSource = gameObject.AddComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.clip = clickSound;
+        audioSource.volume = 0.1f;
     }
 
     void OnMouseDown()
     {
         spriteRenderer.sprite = pressedSprite;
         isPressed = true;
+
+        if (clickSound != null)
+        {
+            audioSource.PlayOneShot(clickSound);
+        }
     }
 
     void OnMouseUp()
@@ -31,17 +42,22 @@ public class CustomButton : MonoBehaviour
             isPressed = false;
         }
     }
+
     public void Continue()
     {
         Debug.Log("Continue");
+        // Дописать события на нажатие
     }
+
     public void NewGame()
     {
         Debug.Log("New Game");
+        // Дописать события на нажатие
     }
+
     public void Exit()
     {
         Debug.Log("Exit");
+        // Дописать события на нажатие
     }
 }
-
